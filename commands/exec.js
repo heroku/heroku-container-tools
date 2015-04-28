b9ce637107fd07584881a72075245b9e999c2ce5
Michael Friis <friism@gmail.com>
2015-04-28 13:24:14 -0700

    make .env work in start also

--- commands/exec.js
+++ commands/exec.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var path = require('path');
 var state = require('../lib/state');
 var docker = require('../lib/docker');
-var dotenv = require('dotenv');
+var envutil = require('../lib/env-util');
 
 module.exports = function(topic) {
   return {
@@ -20,24 +20,7 @@ module.exports = function(topic) {
 };
 
 function runCommand(imageId, cwd, args) {
-  var envParameters = [];
-  try {
-    var dotenvPath = path.join(cwd, '.env');
-    var dotenvEntries = dotenv.parse(fs.readFileSync(dotenvPath));
-    envParameters = Object.keys(dotenvEntries).map(function(key) {
-      return '-e ' + key + '=' + dotenvEntries[key];
-    });
-  }
-  catch (e) {
-    if (e.code === 'ENOENT') {
-      // no .env file
-    }
-    else {
-      throw e;
-    }
-  }
-
-  var envArgComponent = envParameters.join(' ');
+  var envArgComponent = envutil.getFormattedEnvArgComponent(cwd);
   var command = args.join(' ');
   var execString = `docker run -p 3000:3000 -v ${cwd}:/app/src -w /app/src --rm -it ${envArgComponent} ${imageId} ${command} || true`;
   child.execSync(execString, {
--- commands/start.js
+++ commands/start.js
@@ -2,6 +2,7 @@ var path = require('path');
 var child = require('child_process');
 var docker = require('../lib/docker');
 var state = require('../lib/state');
+var envutil = require('../lib/env-util');
 
 var TEMPLATE_PATH = path.resolve(__dirname, '../templates/start-Dockerfile');
 
@@ -13,14 +14,15 @@ module.exports = function(topic) {
     help: `help text for ${topic}:start`,
     run: function(context) {
       var startImageId = docker.ensureStartImage(context.cwd);
-      startImage(startImageId);
+      startImage(startImageId, context.cwd);
     }
   };
 };
 
-function startImage(imageId) {
+function startImage(imageId, cwd) {
   console.log('starting image...');
-  child.execSync(`docker run -p 3000:3000 --rm -it ${imageId} || true`, {
+  var envArgComponent = envutil.getFormattedEnvArgComponent(cwd);
+  child.execSync(`docker run ${envArgComponent} -p 3000:3000 --rm -it ${imageId} || true`, {
     stdio: [0, 1, 2]
   });
 }
