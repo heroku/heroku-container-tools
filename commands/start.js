225b5f6439be2c33e1b8efd3871eb830aeafa8bf
Hunter Loftis <hunter@hunterloftis.com>
2015-04-28 11:32:07 -0700

    log web process URL on docker:start

--- commands/start.js
+++ commands/start.js
@@ -1,7 +1,8 @@
 var path = require('path');
 var child = require('child_process');
+var url = require('url');
+var colors = require('colors');
 var docker = require('../lib/docker');
-var state = require('../lib/state');
 
 var TEMPLATE_PATH = path.resolve(__dirname, '../templates/start-Dockerfile');
 
@@ -19,8 +20,15 @@ module.exports = function(topic) {
 };
 
 function startImage(imageId) {
-  console.log('starting image...');
+  console.log('\nstarting image...');
+  console.log('web process will be available at', colors.yellow.underline(getURL()));
+  
   child.execSync(`docker run -p 3000:3000 --rm -it ${imageId} || true`, {
     stdio: [0, 1, 2]
   });
+}
+
+function getURL() {
+  var host = url.parse(process.env.DOCKER_HOST).hostname;
+  return `http://${host}:3000`;
 }
