ccfe222a6bff583c8cec6bfe8c6e25fd52adbd39
Hunter Loftis <hunter@hunterloftis.com>
2015-04-28 17:36:38 -0700

    running into docker ctrl-c signal bug

--- commands/start.js
+++ commands/start.js
@@ -3,8 +3,8 @@ var child = require('child_process');
 var url = require('url');
 var colors = require('colors');
 var docker = require('../lib/docker');
-var state = require('../lib/state');
-var envutil = require('../lib/env-util');
+var directory = require('../lib/directory');
+var cli = require('heroku-cli-util');
 
 var TEMPLATE_PATH = path.resolve(__dirname, '../templates/start-Dockerfile');
 
@@ -14,22 +14,27 @@ module.exports = function(topic) {
     command: 'start',
     description: 'start docker app container',
     help: 'Start local Docker app container',
+    variableArgs: true,
     run: function(context) {
+      var procfile = directory.readProcfile(context.cwd);
+      if (!procfile) {
+        cli.error('Procfile required. Aborting');
+        return;
+      }
+      var procName = context.args[0] || 'web';
+      var command = procfile[procName];
+      if (!command) {
+        cli.error(`No '${procName}' process type declared in Procfile. Aborting`);
+        return;
+      }
       var startImageId = docker.ensureStartImage(context.cwd);
-      startImage(startImageId, context.cwd);
+
+      console.log('\nstarting image...');
+      console.log('web process will be available at', colors.yellow.underline(getURL()));
+      docker.runImage(startImageId, context.cwd, command, false);
     }
   };
 };
-
-function startImage(imageId, cwd) {
-  if (!imageId) return;
-  console.log('\nstarting image...');
-  console.log('web process will be available at', colors.yellow.underline(getURL()));
-  var envArgComponent = envutil.getFormattedEnvArgComponent(cwd);
-  child.execSync(`docker run ${envArgComponent} -p 3000:3000 --rm -it ${imageId} || true`, {
-    stdio: [0, 1, 2]
-  });
-}
 
 function getURL() {
   var host = url.parse(process.env.DOCKER_HOST).hostname;
