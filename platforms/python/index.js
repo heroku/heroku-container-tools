2d4f5e6af6b9ea1a163610772114ca6f8bf6fda1
Kenneth Reitz <me@kennethreitz.org>
2015-05-05 17:52:49 -0700

    add python platform

---
+++ platforms/python/index.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var path = require('path');
+var _ = require('lodash');
+var exists = require('is-there');
+
+
+module.exports = {
+  name: 'python',
+  detect: function(dir) {
+    if (exists.sync(path.resolve(dir, 'requirements.txt'))) return true;
+    if (exists.sync(path.resolve(dir, 'Gemfile.lock'))) return true;
+  },
+  getDockerfile: function(dir) {
+    var templatePath = path.resolve(__dirname, 'Dockerfile.t');
+    var template = fs.readFileSync(templatePath, { encoding: 'utf8' });
+    var compiled = _.template(template);
+    return compiled({});
+  }
+};
