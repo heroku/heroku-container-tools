f946442273a4ea980382c16f1ae51981b4929ac9
Hunter Loftis <hunter@hunterloftis.com>
2016-06-20 15:04:16 -0400

    restore deprecated commands

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ module.exports = {
   commands: [
     require('./commands/index')(pkg),
     require('./commands/login')(pkg.topic),
-    require('./commands/push')(pkg.topic)
+    require('./commands/push')(pkg.topic),
+    require('./commands/init')(pkg.topic),
+    require('./commands/release')(pkg.topic)
   ]
 };
