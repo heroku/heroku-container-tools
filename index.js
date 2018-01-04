393504e8b142b68fcc36c62f391b08f80a6be0ff
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-04 06:06:36 -0800

    remove commands

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 var pkg = require('./package.json');
 
 module.exports = {
-  commands: [
-    require('./commands/index')(pkg),
-    require('./commands/login')(pkg.topic),
-    require('./commands/push')(pkg.topic),
-    require('./commands/init')(pkg.topic),
-    require('./commands/release')(pkg.topic)
-  ]
+  commands: []
 };
